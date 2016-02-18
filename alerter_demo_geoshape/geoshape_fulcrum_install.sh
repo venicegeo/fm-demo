c1837d5f69c84f1a0b7502111f188e85561a6a8c
Joseph Svrcek <joseph.svrcek@rgi-corp.com>
2016-02-18 16:27:39 -0500

    debugging install script fixed viewer paths attempt3

--- alerter_demo_geoshape/geoshape_fulcrum_install.sh
+++ alerter_demo_geoshape/geoshape_fulcrum_install.sh
@@ -42,6 +42,7 @@ grep -q '^FULCRUM_DATABASE_NAME' /var/lib/geonode/rogue_geonode/geoshape/local_s
 grep -q '^FULCRUM_API_KEY' /var/lib/geonode/rogue_geonode/geoshape/local_settings.py || echo "FULCRUM_API_KEY=''" >> /var/lib/geonode/rogue_geonode/geoshape/local_settings.py
 grep -q '^DATA_FILTERS' /var/lib/geonode/rogue_geonode/geoshape/local_settings.py || echo "DATA_FILTERS=['']" >> /var/lib/geonode/rogue_geonode/geoshape/local_settings.py
 grep -q '^FULCRUM_ASSETS' /var/lib/geonode/rogue_geonode/geoshape/local_settings.py || echo "FULCRUM_ASSETS='/var/lib/geoserver_data/file-service-store'" >> /var/lib/geonode/rogue_geonode/geoshape/local_settings.py
+grep -q '^GEOSHAPE_MEDIA_URL' /var/lib/geonode/rogue_geonode/geoshape/local_settings.py || echo "GEOSHAPE_MEDIA_URL='api/fileservice/view/'" >> /var/lib/geonode/rogue_geonode/geoshape/local_settings.py
 
 #add to /var/lib/geonode/rogue_geonode/geoshape/urls.py:
 grep -qF 'from fulcrum_importer.urls import urlpatterns as fulcrum_importer_urls' /var/lib/geonode/rogue_geonode/geoshape/urls.py || 
