9c5a04dc344f2a57d394c09b1d311a161973e94d
jsvrcek <joseph.svrcek@rgi-corp.com>
2016-03-01 22:00:23 -0500

    remove prompt during disk format

--- vagrant_files/scripts/bootstrap.sh
+++ vagrant_files/scripts/bootstrap.sh
@@ -6,7 +6,7 @@ set -e
 yum -y install http://yum.geoshape.org/geoshape-repo-0.0.1-0.1beta.el6.noarch.rpm
 yum -y install geoshape
 
-mkfs.ext4 /dev/sdb
+mkfs.ext4 -F /dev/sdb
 echo "/dev/sdb                /var/lib/geoserver_data/file-service-store ext4 defaults 1 2" >> /etc/fstab
 mount -a
 chown tomcat:geoservice /var/lib/geoserver_data/file-service-store
