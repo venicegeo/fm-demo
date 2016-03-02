315e545666c6ea495666cc2263cd966b85ed386a
lukerees <luke.rees@rgi-corp.com>
2016-03-02 11:53:06 -0500

    Layer cache now truncates once for each update

--- mvp/fulcrum_importer/tasks.py
+++ mvp/fulcrum_importer/tasks.py
@@ -107,30 +107,22 @@ def handle_file(s3, file_name, file_size):
 
 @shared_task(name="fulcrum_importer.tasks.task_update_tiles")
 def update_tiles(filtered_features, layer_name):
-    for feature in filtered_features.get('features'):
-        if feature.get('geometry').get('type').lower() == 'point':
-            x = feature.get('geometry').get('coordinates')[0]
-            y = feature.get('geometry').get('coordinates')[1]
-            bounds = [str(x - 1),str(y - 1),str(x + 1),str(y + 1)]
-            truncate_tiles(bounds=bounds, layer_name=layer_name.lower(), srs=4326)
-            truncate_tiles(bounds=bounds, layer_name=layer_name.lower(), srs=900913)
+    truncate_tiles(layer_name=layer_name.lower(), srs=4326)
+    truncate_tiles(layer_name=layer_name.lower(), srs=900913)
 
 
-def truncate_tiles(bounds, layer_name=None, srs=None, **kwargs):
+def truncate_tiles(layer_name=None, srs=None, **kwargs):
     import socket
-
     #See http://docs.geoserver.org/stable/en/user/geowebcache/rest/seed.html for more parameters.
     #See also https://github.com/GeoNode/geonode/issues/1656
-
     params = kwargs
     params.setdefault("name", "geonode:{0}".format(layer_name))
-    print layer_name
-    #params.setdefault("bounds", {"coords": {"double": bounds}})
-    #print bounds
     params.setdefault("srs", {"number": srs})
-    print srs
     params.setdefault("zoomStart", 0)
-    params.setdefault("zoomStop", 30)
+    if srs == 4326:
+        params.setdefault("zoomStop", 21)
+    else:
+        params.setdefault("zoomStop", 31)
     params.setdefault("format", "image/png")
     params.setdefault("type", "truncate")
     params.setdefault("threadCount", 4)
@@ -143,7 +135,6 @@ def truncate_tiles(bounds, layer_name=None, srs=None, **kwargs):
         print "Could not get hostname"
         hostname = "https://geoshape.dev"
     url = "https://{0}/geoserver/gwc/rest/seed/geonode:{1}.json".format(hostname, layer_name)
-    print("Truncating cached tiles in extent: {0}".format(bounds))
     print("GWC Payload: {0}".format(payload))
     print("GWC URL: {0}".format(url))
 
